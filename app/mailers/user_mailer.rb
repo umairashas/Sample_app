a02cb7131c7c0a1e03b916a57988a815bf2d987f
umairashas <umairashas@gmail.com>
2025-01-23 18:59:51 +0530

    adding attachment (inline also) in emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,12 +2,16 @@
 class UserMailer < ApplicationMailer
   def welcome_email(user)
     @user = user
+    attachments['download.png'] = File.read(Rails.root.join('public', 'download.png'))  
+    attachments.inline['image.jpg'] = File.read(Rails.root.join('public', 'image.jpg'))
     @url  = 'http://example.com/login'
     mail(to: @user.email, subject: 'Welcome to My Awesome Site')
   end
 
   def first_todo(user)
     @user = user
+    attachments['download.png'] = File.read(Rails.root.join('public', 'download.png'))  
+    attachments.inline['image.jpg'] = File.read(Rails.root.join('public', 'image.jpg'))
     mail(to: @user.email, subject: 'Welcome to My Awesome Site')
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module SampleApp
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
-   
+    config.action_mailer.asset_host = 'http://localhost:3000'  
 
     # Configuration for the application, engines, and railties goes here.
     #
